9f2d19a8c6310b9b88b0fcf4bc95e6adead25f99
anniermeira <202502970641@alunos.estacio.br>
2025-05-20 15:58:55 +0000

    ajustes

--- codar1-2xadrez.c
+++ codar1-2xadrez.c
@@ -4,11 +4,18 @@ int main() {
     int numero;
    
     do{
-        printf("Digite um numero: ");
+        printf("Digite um número PAR para sair do programa...");
+        scanf("%d", &numero);
 
-        printf("O número %d é impar! \n", numero);
+        if(numero % 2 == 0){
+            printf("O número %d é par! \n", numero);
+        } else{
+            printf("O número %d é impar! \n", numero);
+        }
 
-    }while(i % 2 != 0);
+    }while(numero % 2 != 0);
+
+        printf("Você digitou um número PAR, saindo do programa. Tchau!");
    
     return 0;
 }
