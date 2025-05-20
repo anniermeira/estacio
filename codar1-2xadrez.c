fbc3e81034888b7bb7594af0987cf2391cd18118
anniermeira <202502970641@alunos.estacio.br>
2025-05-20 14:30:25 +0000

    segundo cód dentro do primeiro codar

---
+++ codar1-2xadrez.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+    int numero;
+   
+    do{
+        printf("Digite um numero: ");
+
+        printf("O número %d é impar! \n", numero);
+
+    }while(i % 2 != 0);
+   
+    return 0;
+}
