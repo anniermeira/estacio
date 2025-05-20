c88978f0d5351bbfecae2bccf86b9fd452baf00e
anniermeira <202502970641@alunos.estacio.br>
2025-05-20 16:52:19 +0000

    cód torre

---
+++ XADREZnovato.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    //USANDO FOR - TORRE: 5 casas para a direita
+    int torre = 5;
+    printf("Movimento da Torre:\n");
+    for (int i = 0; i < torre; i++) {
+        printf("Direita\n");
+    }
+
+    return 0;
+}
