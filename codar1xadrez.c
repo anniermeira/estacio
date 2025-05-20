b7e992ff0aa2b0c1b2ecf6683fd35ef4e1c6197e
anniermeira <202502970641@alunos.estacio.br>
2025-05-20 12:31:57 +0000

    adicionado instruções

--- codar1xadrez.c
+++ codar1xadrez.c
@@ -5,7 +5,9 @@ int main() {
    
     while (i <= 10) {
         if(i % 2 == 0){
+//podemos colocar a condição (i % 2 != 0) para mostrar os numeros impares
             printf("O número %d é par! \n", i);
+//caso a alteração seja feita, lembre de alterar a imformação no printf
         }
         i++;
     }
