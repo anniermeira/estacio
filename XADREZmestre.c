8aa95fa91d8ba96e1bdad27ef3ad29050623be17
anniermeira <202502970641@alunos.estacio.br>
2025-05-20 20:49:42 +0000

    inicio da escrita nivel mestre

---
+++ XADREZmestre.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//Recursivo - TORRE: Direita
+void moverTorre(int passos) {
+    if (passos <= 0) return;
+    printf("Direita\n");
+    moverTorre(passos - 1);
+}
+
+//Recursivo - RAINHA: Esquerda
+void moverRainha(int passos) {
+    if (passos <= 0) return;
+    printf("Esquerda\n");
+    moverRainha(passos - 1);
+}
+
+//Recursivo - BISPO: Cima, direita
+void moverBispoRecursivo(int passos) {
+    if (passos <= 0) return;
+    printf("Cima Direita\n");
+    moverBispoRecursivo(passos - 1);
+}
+
+// PRINCIPAIS FUNÇÕES
+int main() {
+    
+    return 0;
+}
